61d6b077a3c15e285287b5a2997f0c17c6e96b5d
G00402034 <G00402034@atu.ie>
2023-01-07 17:07:15 +0000

    added prompt asking user for a length

--- src/main/java/ie/atu/ie/atu/week5/ShapeApp.java
+++ src/main/java/ie/atu/ie/atu/week5/ShapeApp.java
@@ -1,4 +1,11 @@
 package ie.atu.ie.atu.week5;
-
+import java.util.Scanner;
 public class ShapeApp {
+
+    public static void main(String[] args) {
+        System.out.println("PLease enter the width of the Rectangle");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+        System.out.println("You entered : " + length);
+    }
 }
